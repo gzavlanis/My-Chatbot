58d25226a6cb967faf003423df5c8d3cd0093603
gzavlanis <gzavlanis@gmail.com>
2024-06-13 15:05:07 +0300

    start creatrting the flask api

--- api/app.py
+++ api/app.py
@@ -1,15 +1,41 @@
-import random
 import numpy as np
 import pickle
 import json
-import nltk
-from flask import Flask, render_template, request
-from flask_ngrok import run_with_ngrok
+from flask import Flask, request
 from keras.models import load_model
-from nltk.stem import WordNetLemmatizer
+from keras.preprocessing.sequence import pad_sequences
 
-Lemmatizer = WordNetLemmatizer()
+model = load_model('../chatbot.keras') # load the trained model
 
-model = load_model("../chatbot.keras")
-intents = json.loads(open("../data/intents.json").read())
-# to be continued...
+with open('../tokenizer.pickle', 'rb') as handle: # load tokenizer object
+    tokenizer = pickle.load(handle)
+
+with open('../label_encoder.pickle', 'rb') as enc: # load the label encoder object
+    lbl_encoder = pickle.load(enc)
+
+with open("../data/intents.json") as file:
+    data = json.load(file)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Welcome to our chatbot!</h1><h4>Application running</h4>"
+
+@app.route("/get", methods = ["POST"])
+def chatbot_response():
+    res = getResponse(data)
+    return res
+
+def getResponse(data):
+    max_len = 20
+    msg = request.form["msg"]
+    result = model.predict(pad_sequences(tokenizer.texts_to_sequences([msg]), truncating = 'post', maxlen = max_len))
+    tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+    for i in data['intents']:
+        if i['tag'] == tag:
+            result = np.random.choice(i['responses'])
+            break
+
+    return result
